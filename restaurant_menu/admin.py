c5beb6b8db7d88b42d0df2c8f402afd9161f528a
antonioconac <conacheantonio13@gmail.com>
2025-09-09 17:17:00 +0300

    Commited Full App - Restaurant Online Menu

---
+++ restaurant_menu/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Item
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal", "status")
+    list_filter = ("status",)
+    search_fields = ("meals", "description")
+
+admin.site.register(Item, MenuItemAdmin)
